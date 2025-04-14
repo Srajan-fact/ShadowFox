e55008a5727e4669354cd5dfaf841183537bfc31
Srajan-fact <phoenixtriibe@gmail.com>
2025-04-14 13:02:51 +0530

    Add files via upload

---
+++ beginner/task1.2.py
@@ -0,0 +1,8 @@
+for =4
+print(for)
+
+#---------------------------------------OUTPUT---------------------------------------------- 
+#File "c:\Users\SRAJAN\Desktop\Shadow fox\beginner\task1.2.py", line 1
+#   for =4
+#        ^
+#SyntaxError: invalid syntax
